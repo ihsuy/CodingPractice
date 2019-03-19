570972b158ff2d2bae9156daaa642652bfb2ed9b
Yushi Qiu <jooehisaishi@gmail.com>
2019-03-19 18:36:05 +0900

    update

---
+++ source/BigButton.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <math.h>
+#include <vector>
+#include <iomanip>
+using namespace std;
+
+bool strLenCmp(const string& s1, const string& s2)
+{
+    return s1.length() < s2.length();
+}
+
+void removePrefixDup(vector<string>& v)
+{
+    sort(v.begin(), v.end(), strLenCmp);
+    for(int i = 0; i < v.size()-1; ++i)
+    {
+        for(int j = i+1; j < v.size(); ++j)
+        {
+            if(v[i] == "" or v[j] == "")
+            {
+                continue;
+            }
+            
+            if(v[i] == v[j].substr(0, v[i].length()))
+            {
+                v[j] = "";
+            }
+        }
+    }
+}
+
+double calculateWinningSeq(const int& N, const vector<string>& p)
+{
+    double totalPoss = pow(2, N);
+    for(int i = 0; i < p.size(); ++i)
+    {
+        if(p[i] != "")
+        {
+            totalPoss -= pow(2, (N-p[i].length()));
+        }
+    }
+    return totalPoss;
+}
+
+
+int main(){
+    int T, N, P;
+    cin >> T;
+    for(int i = 0; i < T; ++i)
+    {
+        cin >> N >> P;
+        vector<string> prefixes;
+        string prefix;
+        for(int j = 0; j < P; ++j)
+        {
+            cin >> prefix;
+            prefixes.push_back(prefix);
+        }
+        removePrefixDup(prefixes);
+
+        cout << "Case #" << i+1 << ": " << setprecision(50) << calculateWinningSeq(N, prefixes) << endl;
+    }
+    return 0;
+}
---
+++ source/Mural.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int chtoi(const char& ch)
+{
+    return ch - '0';
+}
+
+int muralScore(const string& wall, const int& N)
+{
+    int muralWidth = (N+1)/2;
+    
+    int max_score = 0;
+    
+    for(int i = 0; i < muralWidth; ++i)
+    {
+        max_score += chtoi(wall[i]);
+    }
+
+    int temp_score = max_score;
+    cout << temp_score << endl;
+    for(int i = 1; i < N-muralWidth+1; ++i)
+    {
+        temp_score += (chtoi(wall[i+muralWidth-1])-chtoi(wall[i-1]));
+        if(temp_score > max_score)
+        {
+            max_score = temp_score;
+        }
+        cout << temp_score << endl;
+    }
+    
+    return max_score;
+}
+int main()
+{
+    int T, N;
+    cin >> T;
+    for(int i = 0; i < T; ++i)
+    {
+        cin >> N;
+        string wall;
+        cin >> wall;
+        cout << "Case #" << i+1 << ": " << muralScore(wall, N) << endl;
+    }
+    return 0;
+}
