5b115180cbeccc6795bae63700c1fddb3879bacc
Yushi Qiu <jooehisaishi@gmail.com>
2019-03-27 20:52:03 +0900

    quick update

--- source/stringMultiply.cpp
+++ source/stringMultiply.cpp
@@ -125,7 +125,7 @@ string addstr(const string& n1, const string& n2)
 	return result;
 }
 
-string str_mul_char(string n, char c)
+string str_mul_char(const string& n, const char& c)
 {
 	int len = n.length();
 
@@ -134,20 +134,20 @@ string str_mul_char(string n, char c)
 
 	int p = len - 1;
 	int carry = 0, res = 0;
-	while(p >= 0)
+	while (p >= 0)
 	{
-		res = chtoi(c)*chtoi(n[p]) + carry;
+		res = chtoi(c) * chtoi(n[p]) + carry;
 
-		carry = res/10;
+		carry = res / 10;
 
-		res = res%10;
+		res = res % 10;
 
 		result += itoch(res);
 
 		p--;
 	}
 
-	if(carry != 0)
+	if (carry != 0)
 	{
 		result += itoch(carry);
 	}
@@ -160,15 +160,15 @@ string str_mul_char(string n, char c)
 string multiply_helper(const string& num1, const string& num2, const int& len1, const int& len2)
 {
 	string result;
-	result.reserve(len1+len2);
+	result.reserve(len1 + len2);
 	result = "0";
 
 	string zeros;
-	zeros.reserve(len2+1);
+	zeros.reserve(len2 + 1);
 
-	for(int i = len2-1; i >= 0; --i)
+	for (int i = len2 - 1; i >= 0; --i)
 	{
-		result = addstr(result, str_mul_char(num1, num2[i])+zeros);
+		result = addstr(result, str_mul_char(num1, num2[i]) + zeros);
 		zeros += '0';
 	}
 
@@ -177,14 +177,14 @@ string multiply_helper(const string& num1, const string& num2, const int& len1, 
 
 string multiply(const string& num1, const string& num2)
 {
-	if(num1 == "0" or num2 == "0")
+	if (num1 == "0" or num2 == "0")
 	{
 		return "0";
 	}
 
 	int len1 = num1.length(), len2 = num2.length();
 
-	if(len1>=len2)
+	if (len1 >= len2)
 	{
 		return multiply_helper(num1, num2, len1, len2);
 	}
@@ -194,13 +194,87 @@ string multiply(const string& num1, const string& num2)
 	}
 }
 
+
+
+string multiplyFaster_helper(const string& num1, const string& num2, const int& len1, const int& len2)
+{
+	string result(len1 + len2, '0');
+	int res_back = len1 + len2 - 1;
+	for (int i = len2 - 1, count = 0; i >= 0; --i, ++count)
+	{
+		string temp(len1 + 1, '0');
+
+		int carry = 0, res = 0;
+		int t = len1, j = len1 - 1;
+
+		for (; j >= 0; --j, --t)
+		{
+			res = chtoi(num1[j]) * chtoi(num2[i]) + carry;
+
+			carry = res / 10;
+
+			res %= 10;
+
+			temp[t] = itoch(res);
+		}
+
+		temp[t] = itoch(carry);
+
+		// merge to result
+		t = len1;
+		j = res_back - count;
+		carry = 0;
+
+		for (; t >= 0; --t, --j)
+		{
+			res = chtoi(result[j]) + chtoi(temp[t]) + carry;
+
+			carry = res / 10;
+
+			res %= 10;
+
+			result[j] = itoch(res);
+		}
+		if(j>=0)
+		{
+			result[j] = itoch(carry);
+		}
+	}
+
+	if (result[0] == '0')
+	{
+		result.erase(0, 1);
+	}
+
+	return result;
+}
+
+string multiplyFaster(const string& num1, const string& num2)
+{
+	if (num1 == "0" or num2 == "0")
+	{
+		return "0";
+	}
+
+	int len1 = num1.length(), len2 = num2.length();
+
+	if (len1 >= len2)
+	{
+		return multiplyFaster_helper(num1, num2, len1, len2);
+	}
+	else
+	{
+		return multiplyFaster_helper(num2, num1, len2, len1);
+	}
+}
+
 int main()
 {
-	const string n1 = "1110239013784973489251284110239013784973489251284110239013784973489251284110239013784973489251284110239013784973489251284110239013784973489251284";
-	const string n2 = "12349123121231102390137849734892512841102390137849734892512841102390137849734892512841102390137849734892512848918";
-	//string n1 = "0";
-	// string n2 = "1";
-	cout << multiply(n1, n2) << endl;
+	const string n1 = "111023901378497348925128411023901378497348925128411023901378497348925128412512841251284110239013784973489251284110239013784973489251284110239013784973489251284";
+	const string n2 = "123491231212311023901378497348925128411023901378497348925128411023901378214543249734892512841102390137849734892512848918";
+	// // string n1 = "2";
+	// string n2 = "3";
+	cout << multiplyFaster(n1, n2) << endl;
 
 	// const char c = '9';
 
