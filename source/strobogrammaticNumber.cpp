7fb4eb479ed94e92e39602239450834e14056308
Yushi Qiu <jooehisaishi@gmail.com>
2019-04-08 12:25:55 +0900

    quick update

---
+++ source/strobogrammaticNumber.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <vector>
+#include <unordered_set>
+#include <unordered_map>
+#include <set>
+#include <map>
+#include <list>
+#include <chrono>
+#include <random>
+#include <algorithm>
+#include <math.h>
+#include <queue>
+#include <stack>
+#include <sstream>
+#include <utility>
+#include <bitset>
+#include <fstream>
+#include <string>
+
+typedef long long ll;
+inline int two(int n) { return 1 << n; }
+inline int test(int n, int b) { return (n >> b) & 1; }
+inline void set_bit(int & n, int b) { n |= two(b); }
+inline void unset_bit(int & n, int b) { n &= ~two(b); }
+inline int last_bit(int n) { return n & (-n); }
+inline int ones(int n) { int res = 0; while (n && ++res) n -= n & (-n); return res; }
+template<typename T>
+inline void inspect(T& t) {typename T::iterator i1 = t.begin(), i2 = t.end(); while (i1 != i2) {std::cout << (*i1) << ' '; i1++;} std::cout << '\n';}
+
+/////////////////////////////////////////////////////////////
+using namespace std;
+
+/*
+A strobogrammatic number is a number that looks the same when 
+rotated 180 degrees (looked at upside down).
+Write a function to determine if a number is strobogrammatic. 
+The number is represented as a string.
+*/
+
+bool isSymmetricDigit(const char& ch)
+{
+	return (ch == '0' or ch == '1' or ch == '8');
+}
+
+bool isEither6or9(const char& ch1, const char& ch2)
+{
+	return (ch1 == '6' and ch2 == '9') or (ch1 == '9' and ch2 == '6');
+}
+
+bool isStrobogrammatic(string num)
+{
+	int low = 0, high = num.length() - 1;
+	while (low <= high)
+	{
+		if (num[low] != num[high] and not isEither6or9(num[low], num[high]))
+		{
+			return false;
+		}
+		if (not isEither6or9(num[low], num[high]) and (not isSymmetricDigit(num[low]) or not isSymmetricDigit(num[high])))
+		{
+			return false;
+		}
+		low++;
+		high--;
+	}
+	return true;
+}
+
+int main()
+{
+
+
+	return 0;
+}
