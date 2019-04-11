5f817e4a2e79cab7a1be3e4a67c545d6042ded3e
Yushi Qiu <jooehisaishi@gmail.com>
2019-04-11 13:36:47 +0900

    added constant extra space solution and comments

--- source/shortestPalindrome.cpp
+++ source/shortestPalindrome.cpp
@@ -36,8 +36,15 @@ by adding characters in front of it. Find and return the shortest
 palindrome you can find by performing this transformation.
 */
 
+
+// key to this problem:
+// this problem is equivalent to finding the longest palindromic substring of s
+// that starts from the first character of s
+
 string shortestPalindrome1(string s)
 {	// slow
+
+	// bruteforce solution that checks every possible palindromic substring
 	int front = 0, back = s.length() - 1;
 	int prev_back = back;
 	while (front < back)
@@ -57,16 +64,18 @@ string shortestPalindrome1(string s)
 
 	int len = 0;
 	if (front == back)
-	{
+	{	// odd sized case
 		len = front * 2 + 1;
 	}
 	else
-	{
+	{	// even sized case
 		len = front * 2;
 	}
 
 	string prefix = s.substr(len);
+
 	reverse(prefix.begin(), prefix.end());
+
 	return prefix + s;
 }
 
@@ -77,7 +86,8 @@ string shortestPalindrome2(string s)
 
 	int i = 0;
 	for(; i < s.length(); ++i)
-	{
+	{	// instead of creating new string everytime, 
+		// we use underlying iterators to compare them in place
 		if(s.compare(0, s.length()-i, rev_s, i, s.length()-i) == 0)
 		{
 			break;
