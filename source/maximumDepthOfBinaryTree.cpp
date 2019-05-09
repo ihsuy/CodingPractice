87d6743d5bbd63d27df3ca0991332b8257bdc06a
Yushi Qiu <jooehisaishi@gmail.com>
2019-05-09 21:38:21 +0900

    quick update

---
+++ source/maximumDepthOfBinaryTree.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <vector>
+#include <unordered_set>
+#include <unordered_map>
+#include <set>
+#include <map>
+#include <list>
+#include <chrono>
+#include <random>
+#include <algorithm>
+#include <math.h>
+#include <queue>
+#include <stack>
+#include <sstream>
+#include <utility>
+#include <bitset>
+#include <fstream>
+#include <string>
+
+typedef long long ll;
+template<typename T>
+inline void inspect(T& t) {typename T::iterator i1 = t.begin(), i2 = t.end(); while (i1 != i2) {std::cout << (*i1) << ' '; i1++;} std::cout << '\n';}
+
+/////////////////////////////////////////////////////////////
+using namespace std;
+
+/*
+Given a binary tree, find its maximum depth.
+
+The maximum depth is the number of nodes along the longest
+path from the root node down to the farthest leaf node.
+
+Note: A leaf is a node with no children.
+
+Example:
+Given binary tree [3,9,20,null,null,15,7],
+
+    3
+   / \
+  9  20
+    /  \
+   15   7
+return its depth = 3.
+*/
+
+int maxDepth(TreeNode* root)
+{
+    if (not root)
+    {
+        return 0;
+    }
+
+    return max(maxDepth(root->left), maxDepth(root->right)) + 1;
+}
+
+int main()
+{
+    // test cases omitted
+
+    return 0;
+}
