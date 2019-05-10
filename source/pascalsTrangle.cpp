5b8203fc6a92ccc363635cd2a9dae3f311375dd1
Yushi Qiu <jooehisaishi@gmail.com>
2019-05-10 21:52:22 +0900

    quick update

--- source/pascalsTrangle.cpp
+++ source/pascalsTrangle.cpp
@@ -72,6 +72,40 @@ vector<vector<int>> generate(const int& n)
     return p;
 }
 
+/*
+Given a non-negative index k where k ≤ 33, return the kth
+index row of the Pascal's triangle.
+
+Note that the row index starts from 0.
+
+
+In Pascal's triangle, each number is the sum of the two numbers directly above it.
+
+Example:
+
+Input: 3
+Output: [1,3,3,1]
+Follow up:
+
+Could you optimize your algorithm to use only O(k) extra space?
+*/
+
+vector<int> getRow(int n)
+{
+    if (n == 0)
+    {
+        return {1};
+    }
+    vector<int> row(n + 1, 0);
+    row[0] = 1, row.back() = 1;
+    auto prev = getRow(n - 1);
+    for (int i = 1; i < n; ++i)
+    {
+        row[i] = prev[i - 1] + prev[i];
+    }
+    return row;
+}
+
 int main()
 {
 
