4e74c5c1311e2e32e86767367f1110ccfca3f8fe
Yushi Qiu <jooehisaishi@gmail.com>
2019-05-20 12:23:13 +0900

    quick update

---
+++ source/nthDigit.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <vector>
+#include <unordered_set>
+#include <unordered_map>
+#include <set>
+#include <map>
+#include <list>
+#include <chrono>
+#include <random>
+#include <algorithm>
+#include <math.h>
+#include <queue>
+#include <stack>
+#include <sstream>
+#include <utility>
+#include <bitset>
+#include <fstream>
+#include <string>
+
+typedef long long ll;
+template<typename T>
+inline void inspect(T& t) {typename T::iterator i1 = t.begin(), i2 = t.end(); while (i1 != i2) {std::cout << (*i1) << ' '; i1++;} std::cout << '\n';}
+
+/////////////////////////////////////////////////////////////
+using namespace std;
+
+/*
+
+*/
+
+int findNthDigit(int n) {
+    if (n < 10)
+    {
+        return n;
+    }
+    long long k = 9, h = 1, d = 1, s = 9;
+    while (s>0 and s + k * h * 10 * (d + 1) <= n)
+    {
+        h *= 10;
+        d++;
+        s += k * h * d;
+    }
+
+    long long l = n - s;
+    assert(l >= 0);
+    long long i = l / (d + 1), j = l % (d + 1);
+    long long b = pow(10, d) + i - (j > 0 ? 0 : 1);
+    long long g = j > 0 ? (d + 1 - j) : 0;
+    while (g>0 and g--)
+    {
+        b /= 10;
+    }
+    return b % 10;
+}
+
+int main()
+{
+    for(int i = 1; i < 100000; ++i)
+    {
+        cout << findNthDigit(i);
+    }
+
+    return 0;
+}
