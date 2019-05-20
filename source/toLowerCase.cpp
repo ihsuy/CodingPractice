90d73e33af192f02c7847c8262f8007f82bc317c
Yushi Qiu <jooehisaishi@gmail.com>
2019-05-20 13:03:03 +0900

    quick update

---
+++ source/toLowerCase.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <vector>
+#include <unordered_set>
+#include <unordered_map>
+#include <set>
+#include <map>
+#include <list>
+#include <chrono>
+#include <random>
+#include <algorithm>
+#include <math.h>
+#include <queue>
+#include <stack>
+#include <sstream>
+#include <utility>
+#include <bitset>
+#include <fstream>
+#include <string>
+
+typedef long long ll;
+template<typename T>
+inline void inspect(T& t) {typename T::iterator i1 = t.begin(), i2 = t.end(); while (i1 != i2) {std::cout << (*i1) << ' '; i1++;} std::cout << '\n';}
+
+/////////////////////////////////////////////////////////////
+using namespace std;
+
+/*
+Implement function ToLowerCase() that has a string parameter str,
+and returns the same string in lowercase.
+
+Example 1:
+
+Input: "Hello"
+Output: "hello"
+Example 2:
+
+Input: "here"
+Output: "here"
+Example 3:
+
+Input: "LOVELY"
+Output: "lovely"
+*/
+
+string toLowerCase(string str) {
+    for (auto& ch : str)
+    {
+        if (isalpha(ch))
+        {
+            ch = tolower(ch);
+        }
+    }
+    return str;
+}
+
+int main()
+{
+
+
+    return 0;
+}
