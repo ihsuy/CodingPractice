095db3aa143148e9fbd20403609b2236f03d94d7
Yushi Qiu <jooehisaishi@gmail.com>
2019-05-22 15:08:37 +0900

    quick update

---
+++ source/binarySearchTreeToGreaterSumTree.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <vector>
+#include <unordered_set>
+#include <unordered_map>
+#include <set>
+#include <map>
+#include <list>
+#include <chrono>
+#include <random>
+#include <algorithm>
+#include <math.h>
+#include <queue>
+#include <stack>
+#include <sstream>
+#include <utility>
+#include <bitset>
+#include <fstream>
+#include <string>
+
+typedef long long ll;
+template<typename T>
+inline void inspect(T& t) {typename T::iterator i1 = t.begin(), i2 = t.end(); while (i1 != i2) {std::cout << (*i1) << ' '; i1++;} std::cout << '\n';}
+
+/////////////////////////////////////////////////////////////
+using namespace std;
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+
+Given the root of a binary search tree with distinct values, modify it so that every node has a new value equal to the sum of the values of the original tree that are greater than or equal to node.val.
+
+As a reminder, a binary search tree is a tree that satisfies these constraints:
+
+The left subtree of a node contains only nodes with keys less than the node's key.
+The right subtree of a node contains only nodes with keys greater than the node's key.
+Both the left and right subtrees must also be binary search trees.
+ */
+
+int s = 0;
+void backwardTraverse(TreeNode* root)
+{
+    if (not root) return;
+    backwardTraverse(root->right);
+    root->val = s += root->val;
+    backwardTraverse(root->left);
+}
+
+TreeNode* bstToGst(TreeNode* root) {
+    backwardTraverse(root);
+    return root;
+}
+
+int main()
+{
+
+
+    return 0;
+}
