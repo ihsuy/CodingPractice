afd0327e05ef13ef85170de09d0687bdd9c8b0aa
Yushi Qiu <jooehisaishi@gmail.com>
2019-05-27 11:01:26 +0900

    quick update

---
+++ source/hammingDistance.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <vector>
+#include <unordered_set>
+#include <unordered_map>
+#include <set>
+#include <map>
+#include <list>
+#include <chrono>
+#include <random>
+#include <algorithm>
+#include <math.h>
+#include <queue>
+#include <stack>
+#include <sstream>
+#include <utility>
+#include <bitset>
+#include <fstream>
+#include <string>
+
+typedef long long ll;
+template<typename T>
+inline void inspect(T& t) {typename T::iterator i1 = t.begin(), i2 = t.end(); while (i1 != i2) {std::cout << (*i1) << ' '; i1++;} std::cout << '\n';}
+
+/////////////////////////////////////////////////////////////
+using namespace std;
+
+/*
+The Hamming distance between two integers is the number of positions at which the corresponding bits are different.
+
+Given two integers x and y, calculate the Hamming distance.
+
+Note:
+0 ≤ x, y < 231.
+
+Example:
+
+Input: x = 1, y = 4
+
+Output: 2
+
+Explanation:
+1   (0 0 0 1)
+4   (0 1 0 0)
+       ↑   ↑
+
+The above arrows point to positions where the corresponding bits are different.
+*/
+
+int hammingDistance(int x, int y) {
+    x ^= y;
+    int c = 0;
+    for (; x; ++c)
+    {
+        x &= (x - 1);
+    }
+    return c;
+}
+
+int main()
+{
+
+
+    return 0;
+}
