26185afdb20f73f4c8fc5e47c0e868cbefeed9a5
Yushi Qiu <jooehisaishi@gmail.com>
2019-05-30 12:45:17 +0900

    quick update

---
+++ source/wordPattern.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+#include <vector>
+#include <unordered_set>
+#include <unordered_map>
+#include <set>
+#include <map>
+#include <list>
+#include <chrono>
+#include <random>
+#include <algorithm>
+#include <math.h>
+#include <queue>
+#include <stack>
+#include <sstream>
+#include <utility>
+#include <bitset>
+#include <fstream>
+#include <string>
+
+typedef long long ll;
+template<typename T>
+inline void inspect(T& t) {typename T::iterator i1 = t.begin(), i2 = t.end(); while (i1 != i2) {std::cout << (*i1) << ' '; i1++;} std::cout << '\n';}
+
+/////////////////////////////////////////////////////////////
+using namespace std;
+
+/*
+Given a pattern and a string str, find if str follows the same pattern.
+
+Here follow means a full match, such that there is a bijection between a letter in pattern and a non-empty word in str.
+
+Example 1:
+
+Input: pattern = "abba", str = "dog cat cat dog"
+Output: true
+Example 2:
+
+Input:pattern = "abba", str = "dog cat cat fish"
+Output: false
+Example 3:
+
+Input: pattern = "aaaa", str = "dog cat cat dog"
+Output: false
+Example 4:
+
+Input: pattern = "abba", str = "dog dog dog dog"
+Output: false
+*/
+
+bool wordPattern(string pattern, string str) {
+    unordered_map<char, string> buffer;
+    unordered_map<string, char> rbuffer;
+    stringstream ss(str);
+    string word;
+    int i = 0;
+    for (; i < pattern.length() and ss >> word; ++i)
+    {
+        if (buffer.count(pattern[i]) == 0)
+        {
+            if (rbuffer.count(word) != 0)
+            {
+                return false;
+            }
+            buffer[pattern[i]] = word;
+            rbuffer[word] = pattern[i];
+        }
+        else if (buffer[pattern[i]] != word)
+        {
+            return false;
+        }
+    }
+    if (not ss.eof() or i != pattern.length())
+    {
+        return false;
+    }
+    return true;
+}
+
+int main()
+{
+
+
+    return 0;
+}
