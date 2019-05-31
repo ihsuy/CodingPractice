0cc7710853de9ef268edf597043f6bfc5b9b95ca
Yushi Qiu <jooehisaishi@gmail.com>
2019-05-31 10:31:55 +0900

    quick update

---
+++ source/containerWithMostWater.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <vector>
+#include <unordered_set>
+#include <unordered_map>
+#include <set>
+#include <map>
+#include <list>
+#include <chrono>
+#include <random>
+#include <algorithm>
+#include <math.h>
+#include <queue>
+#include <stack>
+#include <sstream>
+#include <utility>
+#include <bitset>
+#include <fstream>
+#include <string>
+
+typedef long long ll;
+template<typename T>
+inline void inspect(T& t) {typename T::iterator i1 = t.begin(), i2 = t.end(); while (i1 != i2) {std::cout << (*i1) << ' '; i1++;} std::cout << '\n';}
+
+/////////////////////////////////////////////////////////////
+using namespace std;
+
+/*
+Given n non-negative integers a1, a2, ..., an , where each represents a point at coordinate (i, ai). n vertical lines are drawn such that the two endpoints of line i is at (i, ai) and (i, 0). Find two lines, which together with x-axis forms a container, such that the container contains the most water.
+
+Note: You may not slant the container and n is at least 2.
+
+Input: [1,8,6,2,5,4,8,3,7]
+Output: 49
+*/
+
+int maxArea(vector<int>& height) {
+    int area = 0;
+    for (int i = 0, j = height.size() - 1; i < j;)
+    {
+        int a = (j - i) * min(height[i], height[j]);
+        if (a > area)
+        {
+            area = a;
+        }
+        if (height[i] < height[j])
+        {
+            i++;
+        }
+        else if (height[i] > height[j])
+        {
+            j--;
+        }
+        else
+        {
+            i++;
+            j--;
+        }
+    }
+    return area;
+}
+
+
+
+int main()
+{
+
+
+    return 0;
+}
