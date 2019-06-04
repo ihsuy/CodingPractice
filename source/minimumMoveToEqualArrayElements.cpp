0444dfaf7d950b813b6d93ef5bd0e9eac320be8c
Yushi Qiu <jooehisaishi@gmail.com>
2019-06-04 20:23:19 +0900

    quick update

---
+++ source/minimumMoveToEqualArrayElements.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <vector>
+#include <unordered_set>
+#include <unordered_map>
+#include <set>
+#include <map>
+#include <list>
+#include <chrono>
+#include <random>
+#include <algorithm>
+#include <math.h>
+#include <queue>
+#include <stack>
+#include <sstream>
+#include <utility>
+#include <bitset>
+#include <fstream>
+#include <string>
+
+typedef long long ll;
+template<typename T>
+inline void inspect(T& t) {typename T::iterator i1 = t.begin(), i2 = t.end(); while (i1 != i2) {std::cout << (*i1) << ' '; i1++;} std::cout << '\n';}
+
+/////////////////////////////////////////////////////////////
+using namespace std;
+
+/*
+Given a non-empty integer array of size n, find the minimum number of moves required to make all array elements equal, where a move is incrementing n - 1 elements by 1.
+
+Example:
+
+Input:
+[1,2,3]
+
+Output:
+3
+
+Explanation:
+Only three moves are needed (remember each move increments two elements):
+
+[1,2,3]  =>  [2,3,3]  =>  [3,4,3]  =>  [4,4,4]
+*/
+
+int minMoves(vector<int>& nums) {
+    long mv = INT_MAX, s = 0;
+    for (auto& n : nums)
+    {
+        s += n;
+        if (n < mv) mv = n;
+    }
+    return s - mv * nums.size();
+}
+
+int main()
+{
+
+
+    return 0;
+}
