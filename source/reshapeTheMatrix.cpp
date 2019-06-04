f808ef29ee53897c7e757248ea48bc6ac6af4f26
Yushi Qiu <jooehisaishi@gmail.com>
2019-06-04 20:22:11 +0900

    quick update

---
+++ source/reshapeTheMatrix.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <vector>
+#include <unordered_set>
+#include <unordered_map>
+#include <set>
+#include <map>
+#include <list>
+#include <chrono>
+#include <random>
+#include <algorithm>
+#include <math.h>
+#include <queue>
+#include <stack>
+#include <sstream>
+#include <utility>
+#include <bitset>
+#include <fstream>
+#include <string>
+
+typedef long long ll;
+template<typename T>
+inline void inspect(T& t) {typename T::iterator i1 = t.begin(), i2 = t.end(); while (i1 != i2) {std::cout << (*i1) << ' '; i1++;} std::cout << '\n';}
+
+/////////////////////////////////////////////////////////////
+using namespace std;
+
+/*
+In MATLAB, there is a very useful function called 'reshape', which can reshape a matrix into a new one with different size but keep its original data.
+
+You're given a matrix represented by a two-dimensional array, and two positive integers r and c representing the row number and column number of the wanted reshaped matrix, respectively.
+
+The reshaped matrix need to be filled with all the elements of the original matrix in the same row-traversing order as they were.
+
+If the 'reshape' operation with given parameters is possible and legal, output the new reshaped matrix; Otherwise, output the original matrix.
+
+Example 1:
+Input: 
+nums = 
+[[1,2],
+ [3,4]]
+r = 1, c = 4
+Output: 
+[[1,2,3,4]]
+Explanation:
+The row-traversing of nums is [1,2,3,4]. The new reshaped matrix is a 1 * 4 matrix, fill it row by row by using the previous list.
+Example 2:
+Input: 
+nums = 
+[[1,2],
+ [3,4]]
+r = 2, c = 4
+Output: 
+[[1,2],
+ [3,4]]
+Explanation:
+There is no way to reshape a 2 * 2 matrix to a 2 * 4 matrix. So output the original matrix.
+*/
+
+vector<vector<int>> matrixReshape(vector<vector<int>>& nums, int r, int c) {
+    int ro = nums.size(), co = nums[0].size(), total = ro * co;
+    if (total != r * c)
+    {
+        return nums;
+    }
+    vector<vector<int>> res(r, vector<int>(c, 0));
+    for (int i = 0; i < total; ++i)
+    {
+        res[i / c][i % c] = nums[i / co][i % co];
+    }
+    return res;
+}
+
+int main()
+{
+
+
+    return 0;
+}
