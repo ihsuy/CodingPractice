0c5181ead645c1dd590a8303cc3143e6b2fb68c5
Yushi Qiu <jooehisaishi@gmail.com>
2019-06-23 22:41:51 +0900

    quick update

---
+++ source/carpooling.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+#include <unordered_set>
+#include <unordered_map>
+#include <set>
+#include <map>
+#include <list>
+#include <chrono>
+#include <random>
+#include <algorithm>
+#include <math.h>
+#include <queue>
+#include <stack>
+#include <sstream>
+#include <utility>
+#include <bitset>
+#include <fstream>
+#include <string>
+
+typedef long long ll;
+template<typename T>
+inline void inspect(T& t){typename T::iterator i1 = t.begin(), i2 = t.end(); while (i1 != i2) {std::cout << (*i1) << ' '; i1++;} std::cout << '\n';}
+
+/////////////////////////////////////////////////////////////
+using namespace std;
+
+/*
+You are driving a vehicle that has capacity empty seats initially available for passengers.  The vehicle only drives east (ie. it cannot turn around and drive west.)
+
+Given a list of trips, trip[i] = [num_passengers, start_location, end_location] contains information about the i-th trip: the number of passengers that must be picked up, and the locations to pick them up and drop them off.  The locations are given as the number of kilometers due east from your vehicle's initial location.
+
+Return true if and only if it is possible to pick up and drop off all passengers for all the given trips. 
+*/
+bool carPooling(vector<vector<int>>& trips, int capacity) {
+        vector<int> k(1001, 0);
+        for(int i = 0; i < trips.size(); ++i)
+        {
+            for(int j = trips[i][1]; j < trips[i][2]; ++j)
+            {
+                k[j] += trips[i][0];
+                if(k[j]>capacity)
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+
+int main()
+{
+
+
+	return 0;
+}
